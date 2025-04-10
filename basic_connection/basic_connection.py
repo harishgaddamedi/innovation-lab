a8bcb225c628192c5257d56e08d22b3360d5e27e
apuuhafeez <sahrooappu@gmail.com>
2025-04-10 18:08:51 +0530

    test

--- basic_connection/basic_connection.py
+++ basic_connection/basic_connection.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyC45EQ3pQM74h6BKKyOYvq8DP01qHVGTGc")
+client = genai.Client(api_key="")
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works in a few words"
