2578d6270042a9f0f3c4cf99535c1c413b9361e0
apuuhafeez <sahrooappu@gmail.com>
2025-04-10 18:46:03 +0530

    added agentic flows

--- agentic_workflows/agentic_workflow.py
+++ agentic_workflows/agentic_workflow.py
@@ -3,59 +3,79 @@ from langgraph.graph import StateGraph, START, END
 from typing_extensions import TypedDict, Literal
 import uuid
 import re
+from google import genai
 
+client = genai.Client(api_key="")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+)
 
 class MessageState(TypedDict):
+    essay: str
+    score: str
     messages: str
 
 def get_content(data):
-    print("data: "+data)
-    return data
+    print("======util======")
+    print("before:"+data)
+    decoded_text = bytes(data, "utf-8").decode("unicode_escape")
+    return decoded_text
 
-def agent_1(state: MessageState) -> Command[Literal["agent_2","agent_3",END]]:
-    print("agent 1")
+
+def essay_writer(state: MessageState) -> Command[Literal["essay_scorer",END]]:
+    print("essay_writer")
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents="Write an essay of 100 words about AI your essay should container lot technical details on how AI works"
+    )
+    #print("essay: "+str(response.text))
+    state["essay"] = str(response.text)
     return Command(
-        goto="agent_2",
-        update={"messages":"agent 1 completed"}
+        goto="essay_scorer",
+        update={"essay":str(response.text)}
     )
 
-def agent_2(state: MessageState) -> Command[Literal["agent_1","agent_3",END]]:
-    print("agent 2")
-    content = get_content(state["messages"])
-    if "Continue" == content:
-        state["messages"] = "Stop" 
-        return Command(
-            goto="agent_3",
-            update={"messages":"Stop"}
-        )
-    else:
-        return Command(
-            goto="agent_3",
-            update={"messages":"agent 2 completed"}
-        )
+def essay_scorer(state: MessageState) -> Command[Literal["essay_decision_maker",END]]:
+    print("essay_scorer")
 
-def agent_3(state: MessageState) -> Command[Literal["agent_1","agent_3",END]]:
-    print("agent 3")
-    content = get_content(state["messages"])
-    if "Stop" == content:
+    essay = get_content(state["essay"])    
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents="Score the given essay below on scale of 1 to 50 based on how good information is and how good the language. Your response should only include number from 1 to 50 nothing else should in response \n Essay: \n"+essay
+    )
+    #print("score"+str(response.text))
+    state["score"] = str(response.text)
+
+    return Command(
+            goto="essay_decision_maker",
+            update={"score":str(response.text)}
+    )
+
+def essay_decision_maker(state: MessageState) -> Command[Literal["essay_writer",END]]:
+    print("essay_decision_maker")
+    content = get_content(state["score"])
+    print("score: "+content)
+    score = int(get_content(state["score"]))
+    if score > 25:
         return Command(
             goto=END ,
-            update={"messages":"agent 3 completed"}
+            update={"messages":"essay_decision_maker completed"}
         )
     else:
         state["messages"] = "Continue"
         return Command(
-            goto="agent_2",
+            goto="essay_writer",
             update={"messages":"Continue"}
         )
 
 builder = StateGraph(MessageState)
-builder.add_node(agent_1)
-builder.add_node(agent_2)
-builder.add_node(agent_3)
+builder.add_node(essay_writer)
+builder.add_node(essay_scorer)
+builder.add_node(essay_decision_maker)
 
-builder.add_edge(START, "agent_1")
-builder.add_edge("agent_3",END)
+builder.add_edge(START, "essay_writer")
+builder.add_edge("essay_decision_maker",END)
 
 network = builder.compile()
 config = {
@@ -63,7 +83,7 @@ config = {
         "thread_id": uuid.uuid4()
     }
 }
-state = MessageState(messages=[""])
+state = MessageState(messages="", essay="",score="")
 for chunk in network.stream(state,config):
     print(chunk)
     print("=========")
